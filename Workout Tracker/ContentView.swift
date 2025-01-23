17f9ff45af62891eaa9b4fc4d08d6f67ab793685
Haris Alicic <haris.alicic1@gmail.com>
2025-01-23 06:48:42 +0100

    Initial Commit

---
+++ Workout Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout Tracker
+//
+//  Created by Haris Alicic on 23. 1. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Haris Alicic on 23. 1. 2025..
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
